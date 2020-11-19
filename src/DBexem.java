db52c47d4e0e0aaded8df42b3b5fa34b45cfcd56
jorker08 <jorker08@naver.com>
2020-11-19 12:53:27 +0900

    201119

--- src/DBexem.java
+++ src/DBexem.java
@@ -3,46 +3,112 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class DBexem {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// db 접속 정보
+		ArticleDao articleDao = new ArticleDao();
+		Scanner sc = new Scanner(System.in);
+
 		
-		//드라이버 정보
-		String driver = "com.mysql.cj.jdbc.Driver";
-		
-		//dbms 정보
-		String url = "jdbc:mysql://localhost:3306/t1?serverTimeZone=UTC";
-		
-		//사용자 계정
-		String user = "sbsst";
-		
-		//사용자 비밀번호
-		String pass = "sbs123414";
-		
-		Class.forName(driver);// Driver 세팅
-		Connection conn = DriverManager.getConnection(url, user, pass); // DBMS 선택 -> 담당자(Connection);
-		String sql = "SELECT * FROM article";
-		PreparedStatement pstmt = conn.prepareStatement(sql); // preparedStatment 통해서 sql 세팅
-		
-		ResultSet rs = pstmt.executeQuery(); // 조회 결과가 있는경우
-//		pstmt.executeUpdate();// 조회 결과가 없는경우
-		rs.next(); // 데이터가 있으면 true, 없으면 flase
-		
-		while(rs.next()) {
-			int id = rs.getInt("id");
-			String title = rs.getString("title");
-			String body = rs.getString("body");
-			String nickname = rs.getString("nickname");
-			int hit = rs.getInt("hit");
-			System.out.println(id+ " " + title + " " + body + " " + nickname + " " + hit);
+
+		while (true) {
+			System.out.println("명령어를 입력해주세요 : ");
+			String hdw = sc.nextLine();
+
+			if (hdw.equals("add")) {
+				System.out.println("제목 : ");
+				String title = sc.nextLine();
+				System.out.println("내용 : ");
+				String body = sc.nextLine();
+				System.out.println("작성자 : ");
+				String nickname = sc.nextLine();
+				System.out.println("등록날짜 : ");
+				String date = sc.nextLine();
+
+				String sql = "INSERT INTO article SET title = ?, `body` = ?, nickname = ?, `date` = ?, hit = ?";
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+
+				pstmt.setString(1, title);
+				pstmt.setString(2, body);
+				pstmt.setString(3, nickname);
+				pstmt.setString(4, date);
+
+				pstmt.executeUpdate();
+
+				System.out.println("등록했습니다.");
+			} else if (hdw.equals("list")) {
+				String sql2 = "SELECT*FROM article";
+
+				PreparedStatement pstmt = conn.prepareStatement(sql2);
+				ResultSet rs = pstmt.executeQuery();
+
+				while (rs.next()) {
+					String title = rs.getString("title");
+					int id = rs.getInt("id");
+					String body = rs.getString("body");
+					String nickname = rs.getString("nickname");
+					String date = rs.getString("date");
+					int hit = rs.getInt("hit");
+					System.out.println(id + " " + title + " " + body + " " + nickname + " " + date + " " + hit);
+				}
+
+			}else if(hdw.equals("read")) {
+				System.out.print("게시물 번호를 입력해주세요 : ");
+				int id = sc.nextInt();
+				String sql3 = "SELECT*FROM article WHERE id = ? ";
+				
+
+				PreparedStatement pstmt = conn.prepareStatement(sql3);
+				ResultSet rs = pstmt.executeQuery();
+				
+				pstmt.setInt(1, id);
+				
+				if (rs.next()) {
+					String title = rs.getString("title");
+					int id2 = rs.getInt("id");
+					String body = rs.getString("body");
+					String nickname = rs.getString("nickname");
+					String date = rs.getString("date");
+					int hit = rs.getInt("hit");
+					System.out.println(id2 + " " + title + " " + body + " " + nickname + " " + date + " " + hit);
+				}else {
+					System.out.println("없는게시물 입니다.");
+				}
+			}
+			else if(hdw.equals("update")) {
+				System.out.println("번호 : ");
+				int id = sc.nextInt();
+				String sql4 = "UPDATE article SET title = ? WHERE id = ?";
+				PreparedStatement pstmt = conn.prepareStatement(sql4);
+				
+				System.out.println("제목 : ");
+				String title = sc.nextLine();
+				System.out.println("내용 : ");
+				String body = sc.nextLine();
+				
+				
+				pstmt.setString(1, title);
+				pstmt.setString(2, body);
+				pstmt.setInt(3, id);
+				
+				pstmt.executeUpdate();
+				
+
+				
+			}else if(hdw.equals("delete")) {
+				System.out.println("삭제할 게시물을 선택해주세요");
+				int id = sc.nextInt();
+				
+				String sql5 = "DELETE FROM article WHERE id = ?";
+				PreparedStatement pstmt = conn.prepareStatement(sql5);
+				pstmt.setInt(1, id);
+				
+				pstmt.executeUpdate();
+				
+			}
 		}
-		
-		String sql2 = "INSERT INTO article SET title = 'bbb', `body` = 'ccc', nickname = '홍길동', hit = 10;";
-		
-		pstmt.executeUpdate(sql2);
-		
 	}
 
 }
